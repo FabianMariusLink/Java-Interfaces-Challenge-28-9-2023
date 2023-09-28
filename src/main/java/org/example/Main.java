60bfb859ead7b5a9741c4baf499abc6f494c14ff
FabianMariusLink <fabian.marius.link@gmail.com>
2023-09-28 11:26:45 +0200

    Task 5 instantiate objects musicPlayer1, musicPlayer1 with Playable

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        MediaController mediaController1 = new MediaController();
+        Playable videoPlayer1 = new VideoPlayer();
+        Playable musicPlayer1 = new MusicPlayer();
+        mediaController1.playMedia(videoPlayer1);
+        mediaController1.playMedia(musicPlayer1);
+        // mediaController1.playMedia(new VideoPlayer());
+        // mediaController1.playMedia(new MusicPlayer());
     }
 }
